5981b3c3a55c0494da64ee171a0faa0f5414d80e
LAPTOP-5SS2J05M\user <likjhee1@gmail.com>
2022-07-11 14:16:16 +0900

    에셋관리 + 풀메니저
    에셋관리 + 풀메니저

---
+++ Assets/04_Pooling/PoolingTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingTest : MonoBehaviour
+{
+    private void Update()
+    {
+        PoolingTestCode();
+    }
+
+
+
+    void PoolingTestCode()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(1);
+            GameObject testObj=PoolingManager._Instance.PopObj("TestObj");
+        }
+    }
+
+
+}
